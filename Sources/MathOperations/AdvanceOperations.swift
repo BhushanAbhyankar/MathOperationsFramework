2b723484289b7e1cccc4875e166321b421048be7
BhushanAbhyankar <bhushan25abhyankar@gmail.com>
2024-06-04 20:47:57 +0100

    open acess speifier

---
+++ Sources/MathOperations/AdvanceOperations.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bhushan Abhyankar on 04/06/2024.
+//
+
+import Foundation
+open class AdvanceOperations{
+    
+    
+    public init() {}
+    
+    open func doAverage(_ a: Double, _ b: Double) -> Double {
+        let result = a + b / 2
+        return result
+    }
+}
